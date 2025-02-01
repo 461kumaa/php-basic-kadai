a0a7d76e21ce2972580b7a5d987f8731644e23a4
461kumaa <comnom55@gmail.com>
2025-02-01 22:02:34 +0900

    kadai_005をアップロード

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -21,7 +21,9 @@
 
     $sum = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
     
-    echo $sum / 10;
+    $average = $sum / 10;
+
+    echo $average;
     ?>
   </p>
 
